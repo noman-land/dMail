28c0232bb535ba8a5e8d1c06fccef5ff3bdd2166
Noman <noman@noman.land>
2016-11-13 06:24:14 -0500

    Add ipfs go online actions

---
+++ js/actions/ipfsActions.js
@@ -0,0 +1,14 @@
+import { createAction } from 'redux-actions';
+import ActionTypes from './actionTypes';
+
+export const goOnlineStart = createAction(
+  ActionTypes.IPFS_GO_ONLINE_START
+);
+
+export const goOnlineSuccess = createAction(
+  ActionTypes.IPFS_GO_ONLINE_SUCCESS
+);
+
+export const goOnlineError = createAction(
+  ActionTypes.IPFS_GO_ONLINE_ERROR
+);
