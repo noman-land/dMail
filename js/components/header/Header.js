674d0c10b5a304b21507600563c38b030625eba8
Noman <Noman>
2016-11-13 01:30:14 -0500

    Make OnlineBadge component

--- js/components/header/Header.js
+++ js/components/header/Header.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import classNames from 'classnames';
+
+import OnlineBadge from './OnlineBadge';
 
 class Header extends Component {
   constructor(props, context) {
@@ -11,36 +12,21 @@ class Header extends Component {
     const { messages, connectedToEthereum, connectedToIPFS } = this.props;
     return (
       <div className="header flex justify-space-between p-5">
-        <div>
-          dMail
+        <div className="flex align-items-center">
+          <span>
+            dMail
+          </span>
           <span className="circle p-3 text-white m-2-l bg-green">
             {messages.length}
           </span>
         </div>
-        <div className="flex">
-          <div className={classNames(
-            {
-              'bg-green': connectedToEthereum,
-              'bg-red': !connectedToEthereum,
-            },
-            'circle',
-            'p-2',
-            'text-white',
-          )}>
+        <div className="flex align-items-center">
+          <OnlineBadge isOnline={connectedToEthereum}>
             ETH
-          </div>
-          <div className={classNames(
-            {
-              'bg-green': connectedToIPFS,
-              'bg-red': !connectedToIPFS,
-            },
-            'circle',
-            'm-2-l',
-            'p-2',
-            'text-white',
-          )}>
+          </OnlineBadge>
+          <OnlineBadge isOnline={connectedToIPFS}>
             IPFS
-          </div>
+          </OnlineBadge>
         </div>
       </div>
     );
