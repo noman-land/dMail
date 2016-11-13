33ba52c86f836d1c899606f0e266f03c78a9b878
Noman <Noman>
2016-11-13 01:12:11 -0500

    Add online static bubbles for Ethereum and IPFS

--- js/components/Header.js
+++ js/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import classNames from 'classnames';
 
 class Header extends Component {
   constructor(props, context) {
@@ -9,11 +10,38 @@ class Header extends Component {
   render() {
     const { messages, connectedToEthereum, connectedToIPFS } = this.props;
     return (
-      <div className="header flex p-5">
-        dMail
-        <span className="circle p-3 text-white m-2-l" style={{background: 'red'}}>
-          {messages.length}
-        </span>
+      <div className="header flex justify-space-between p-5">
+        <div>
+          dMail
+          <span className="circle p-3 text-white m-2-l bg-green">
+            {messages.length}
+          </span>
+        </div>
+        <div className="flex">
+          <div className={classNames(
+            {
+              'bg-green': connectedToEthereum,
+              'bg-red': !connectedToEthereum,
+            },
+            'circle',
+            'p-2',
+            'text-white',
+          )}>
+            ETH
+          </div>
+          <div className={classNames(
+            {
+              'bg-green': connectedToIPFS,
+              'bg-red': !connectedToIPFS,
+            },
+            'circle',
+            'm-2-l',
+            'p-2',
+            'text-white',
+          )}>
+            IPFS
+          </div>
+        </div>
       </div>
     );
   }
