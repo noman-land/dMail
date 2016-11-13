7c8fa8d4fd4cbd724660fd2c807a2c684c257528
Noman <Noman>
2016-11-13 06:28:02 -0500

    Add addJson and getJson methods to ipfsUtils

--- js/modules/ipfsUtils.js
+++ js/modules/ipfsUtils.js
@@ -1,5 +1,6 @@
 import IPFS from 'ipfs';
 import Q from 'q';
+import { Buffer } from 'buffer';
 window.ipfs = new IPFS('ipfs'); // this is the name of the indexDB database
 
 export const goOnline = () => {
@@ -37,5 +38,30 @@ export const goOnline = () => {
     });
   });
 
+  return deferred.promise;
+};
+
+export const addJson = (file) => {
+  const jsonBuffer = new Buffer(JSON.stringify(file));
+  return Q(ipfs.files.add(jsonBuffer)).get(0).get('path');
+};
+
+export const getJson = (ipfsHash) => {
+  const deferred = Q.defer();
+  ipfs.files.cat(ipfsHash).then(stream => {
+    let buffer = [];
+    stream.on('error', error => {
+      deferred.reject(error);
+    });
+
+    stream.on('data', chunk => {
+      buffer.push(chunk);
+    });
+
+    stream.on('end', () => {
+      const mail = JSON.parse(buffer.toString());
+      deferred.resolve(mail);
+    })
+  }, deferred.reject);
   return deferred.promise;
 };
