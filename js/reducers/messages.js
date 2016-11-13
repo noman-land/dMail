df99d5e0a149299b5b52c282e88778378f876ee6
Noman <noman@noman.land>
2016-11-12 23:21:16 -0500

    Add messages fetching actions

---
+++ js/reducers/messages.js
@@ -0,0 +1,21 @@
+import ActionTypes from '../actions/actionTypes';
+
+const messages = (
+  state = [{
+    subject: 'RE: Hello',
+    body: 'Hello',
+  }, {
+    subject: 'FWD: Goodbye',
+    body: 'Goodbye',
+  }],
+  action
+) => {
+  switch (action.type) {
+    case ActionTypes.MESSAGES_FETCH_SUCCESS:
+      return action.payload.messages;
+    default:
+      return state;
+  }
+};
+
+export default messages;
