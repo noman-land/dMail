6813e56a60716f75720d05b4590cd82229766b6e
Noman <Noman>
2016-11-12 23:11:38 -0500

    Add Footer component and sass

---
+++ js/components/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class Footer extends Component {
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  render() {
+    return (
+      <div className="footer flex p-5">
+        Footer
+      </div>
+    );
+  }
+}
