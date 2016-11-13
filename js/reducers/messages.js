fd3e1f61233d0b69e29dce721764c66b04a825a9
Noman <Noman>
2016-11-13 17:09:29 -0500

    Add some more default emails

--- js/reducers/messages.js
+++ js/reducers/messages.js
@@ -1,13 +1,35 @@
 import ActionTypes from '../actions/actionTypes/messagesActionTypes';
 
-const messages = (
-  state = [{
+const defaultState = [
+  {
     subject: 'RE: Hello',
     body: 'Hello',
   }, {
     subject: 'FWD: Goodbye',
     body: 'Goodbye',
-  }],
+  }, {
+    subject: 'An email from a friend',
+    body: 'Want to hang out?',
+  }, {
+    subject: 'New car available',
+    body: 'Come get it',
+  }, {
+    subject: 'FRE3 V1AGRA',
+    body: 'THS NOT SPM',
+  }, {
+    subject: 'This is not spam',
+    body: 'For real',
+  }, {
+    subject: 'Becky wants to talk',
+    body: 'It\'s important',
+  }, {
+    subject: 'More definitely not spam',
+    body: 'Really though',
+  },
+];
+
+const messages = (
+  state = defaultState,
   action
 ) => {
   switch (action.type) {
