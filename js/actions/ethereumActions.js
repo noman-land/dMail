be00db13d7815dedadf5a29adaf6c16d6b4c4263
Noman <noman@noman.land>
2016-11-13 18:37:05 -0500

    Acphabetize actions

--- js/actions/ethereumActions.js
+++ js/actions/ethereumActions.js
@@ -1,6 +1,10 @@
 import { createAction } from 'redux-actions';
 import ActionTypes from './actionTypes/ethereumActionTypes';
 
+export const goOnlineError = createAction(
+  ActionTypes.ETHEREUM_GO_ONLINE_ERROR
+);
+
 export const goOnlineStart = createAction(
   ActionTypes.ETHEREUM_GO_ONLINE_START
 );
