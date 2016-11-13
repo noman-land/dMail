5b5691896d0373f212304c8ac2e126887024e415
Noman <noman@noman.land>
2016-11-13 16:59:21 -0500

    Remove unused 'address' reducer and action

--- js/reducers/rootReducer.js
+++ js/reducers/rootReducer.js
@@ -1,14 +1,12 @@
 import activeMessage from './activeMessage';
 import connectedToEthereum from './connectedToEthereum';
 import connectedToIPFS from './connectedToIPFS';
-import address from './address';
 import messages from './messages';
 
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
   activeMessage,
-  address,
   connectedToEthereum,
   connectedToIPFS,
   messages,
