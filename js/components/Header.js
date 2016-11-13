6132c1766dce1a6fd43bd3d10676c27f821f6614
Noman <noman@noman.land>
2016-11-12 23:26:13 -0500

    Add Header component and styles

---
+++ js/components/Header.js
@@ -0,0 +1,24 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+
+class Header extends Component {
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  render() {
+    const { messages } = this.props;
+    return (
+      <div className="header flex p-5">
+        dMail
+        <span className="circle p-3 text-white m-2-l" style={{background: 'red'}}>
+          {messages.length}
+        </span>
+      </div>
+    );
+  }
+}
+
+export default connect(
+  state => ({messages: state.messages})
+)(Header);
