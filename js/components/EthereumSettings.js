70102d5f0e0c34f4d48fb455db00efa1d14a3632
Noman <Noman>
2016-11-13 19:15:55 -0500

    Connect ethereumAccounts to EthereumSettings

--- js/components/EthereumSettings.js
+++ js/components/EthereumSettings.js
@@ -7,21 +7,41 @@ class EthereumSettings extends Component {
   }
 
   render() {
+    const { accounts } = this.props;
+
     return (
-      <div className="h1">
-        Ethereum Settings!
+      <div className="p-5">
+        <h1>
+          Ethereum Settings!
+        </h1>
+        {accounts && (
+          <div>
+            <h3>
+              You have {accounts.length} Ethereum addresses
+            </h3>
+            <ul className="list-style-none">
+              {accounts.map(account => (
+                <li key={account}>
+                  {account}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
     );
   }
 }
 
 EthereumSettings.propTypes = {
+  accounts: PropTypes.arrayOf(PropTypes.string),
   web3: PropTypes.object.isRequired
 };
 
 export default connect(
-  () => {
+  state => {
     return {
+      accounts: state.ethereumAccounts,
       web3: window.web3,
     }
   }
