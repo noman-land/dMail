d496486dfe7b8061796d47724942e342753b6786
Noman <Noman>
2016-11-13 19:12:29 -0500

    Add Ethereum accounts fetching actions

--- js/actions/ethereumActions.js
+++ js/actions/ethereumActions.js
@@ -13,6 +13,14 @@ export const goOnlineSuccess = createAction(
   ActionTypes.ETHEREUM_GO_ONLINE_SUCCESS
 );
 
-export const goOnlineError = createAction(
-  ActionTypes.ETHEREUM_GO_ONLINE_ERROR
+export const fetchAccountsError = createAction(
+  ActionTypes.ETHEREUM_FETCH_ACCOUNTS_ERROR
+);
+
+export const fetchAccountsStart = createAction(
+  ActionTypes.ETHEREUM_FETCH_ACCOUNTS_START
+);
+
+export const fetchAccountsSuccess = createAction(
+  ActionTypes.ETHEREUM_FETCH_ACCOUNTS_SUCCESS
 );
