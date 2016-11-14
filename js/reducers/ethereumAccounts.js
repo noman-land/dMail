ae555ca561a77a1a499534311f8de9ba03e64856
Noman <Noman>
2016-11-13 19:16:47 -0500

    Remove console.log()

--- js/reducers/ethereumAccounts.js
+++ js/reducers/ethereumAccounts.js
@@ -4,9 +4,6 @@ const ethereumAccounts = (
   state = null,
   action
 ) => {
-  console.log(action);
-
-
   switch (action.type) {
     case ActionTypes.ETHEREUM_FETCH_ACCOUNTS_SUCCESS:
       return action.payload.accounts;
