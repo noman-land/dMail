81c7516b9b52318b2cc0ee25c29797f4e96b1485
Noman <noman@noman.land>
2016-11-13 22:06:13 -0500

    Add mailboxes reducer

---
+++ js/reducers/mailboxes.js
@@ -0,0 +1,13 @@
+import ActionTypes from '../actions/actionTypes/ethereumActionTypes';
+
+const mailboxes = (
+  state = [],
+  action
+) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default mailboxes;
