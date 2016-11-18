3e128ad951a726af252b1c740fca207b3bf22b0e
Noman <noman@noman.land>
2016-11-18 00:39:40 -0500

    Alphabetize

--- js/reducers/rootReducer.js
+++ js/reducers/rootReducer.js
@@ -1,22 +1,22 @@
-import addingMailbox from './addingMailbox';
+import activeAccount from './activeAccount';
 import activeMailbox from './activeMailbox';
 import activeMessage from './activeMessage';
+import addingMailbox from './addingMailbox';
 import connectedToEthereum from './connectedToEthereum';
 import connectedToIPFS from './connectedToIPFS';
 import ethereumAccounts from './ethereumAccounts';
-import activeAccount from './activeAccount';
 import messages from './messages';
 
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
   activeMessage,
+  activeAccount,
   activeMailbox,
   addingMailbox,
   connectedToEthereum,
   connectedToIPFS,
   ethereumAccounts,
-  activeAccount,
   messages,
 });
 
