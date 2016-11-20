75c3dd0e5a85455600d17190d99284a0ce55bb61
Noman <Noman>
2016-11-20 18:51:22 -0500

    Add links to Header

--- js/components/header/Header.js
+++ js/components/header/Header.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router';
 
 import EthereumOnlineBadge from './EthereumOnlineBadge';
 import IpfsOnlineBadge from './IpfsOnlineBadge';
@@ -14,16 +15,22 @@ class Header extends Component {
     return (
       <div className="header">
         <div className="flex align-items-center">
-          <span>
-            dMail
-          </span>
-          <span className="circle p-3 text-white m-2-l bg-green">
-            {messagesLength}
-          </span>
+          <Link to="inbox" className="decoration-none">
+            <span className="text-black">
+                dMail
+            </span>
+            <span className="circle p-3 text-white m-2-l bg-green">
+              {messagesLength}
+            </span>
+          </Link>
         </div>
         <div className="flex align-items-center">
-          <EthereumOnlineBadge />
-          <IpfsOnlineBadge />
+          <Link to="settings" className="decoration-none">
+            <EthereumOnlineBadge />
+          </Link>
+          <Link to="settings" className="decoration-none">
+            <IpfsOnlineBadge />
+          </Link>
         </div>
       </div>
     );
