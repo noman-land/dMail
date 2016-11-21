ebdde82e332bc9ebb24a54f092477e92d98894b7
Noman <noman@noman.land>
2016-11-21 01:34:17 -0500

    Make MessageSnippets clickable

--- js/components/inbox/MessageSnippet.js
+++ js/components/inbox/MessageSnippet.js
@@ -1,17 +1,45 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { Link } from 'react-router';
 
-const MessageSnippet = ({ message: { subject } }) => {
-  return (
-    <li className="message-snippet">
-      <div>
-        {subject}
-      </div>
-    </li>
-  );
-};
+class MessageSnippet extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    const {
+      message,
+      setActiveMessage,
+    } = this.props;
+
+    setActiveMessage(message);
+  }
+
+  render() {
+    const {
+      message: {
+        id,
+        subject,
+        body
+      },
+      pathname,
+    } = this.props;
+
+    return (
+      <li className="message-snippet" onClick={this.handleClick}>
+        <Link to={`${pathname}/${id}`} className="decoration-none text-black">
+          {subject}: {body}
+        </Link>
+      </li>
+    );
+  }
+}
 
 MessageSnippet.propTypes = {
-  message: PropTypes.object.isRequired
+  message: PropTypes.object.isRequired,
+  pathname: PropTypes.string.isRequired,
+  setActiveMessage: PropTypes.func.isRequired,
 };
 
 export default MessageSnippet;
