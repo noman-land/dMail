82cfb6ae223f2d1e447fcc1ef6d7719dd1bd47c1
Noman <noman@noman.land>
2016-11-21 01:38:50 -0500

    Make MessageFull a class and make it a container

--- js/components/inbox/MessageFull.js
+++ js/components/inbox/MessageFull.js
@@ -1,20 +1,37 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 
-const MessageFull = ({ message: { subject, body } }) => {
-  return (
-    <div>
+class MessageFull extends Component {
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  componentDidMount() {
+  }
+
+  render() {
+    const {
+      message: {
+        subject,
+        body,
+      },
+    } = this.props;
+
+    return (
       <div>
-        Subject: {subject}
+        <div>
+          Subject: {subject}
+        </div>
+        <div>
+          Body: {body}
+        </div>
       </div>
-      <div>
-        Body: {body}
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 MessageFull.propTypes = {
-  message: PropTypes.object.isRequired
+  message: PropTypes.object.isRequired,
+  routeParams: PropTypes.object.isRequired,
 };
 
 export default MessageFull;
