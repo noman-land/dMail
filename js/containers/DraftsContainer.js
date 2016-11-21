b5f485ccc6493da33ec9ba536b72734fe232d779
Noman <Noman>
2016-11-21 01:36:09 -0500

    Add DraftsContainer to wrap generic MessageList

---
+++ js/containers/DraftsContainer.js
@@ -0,0 +1,18 @@
+import React, { PropTypes, Component } from 'react';
+import { connect } from 'react-redux';
+
+import MessageList from '../components/inbox/MessageList';
+
+import { setActiveMessage } from '../actions/messagesActions';
+
+export default connect(
+  (state, props) => ({
+    messages: state.messages.slice(4),
+    pathname: props.location.pathname,
+  }),
+  dispatch => ({
+    setActiveMessage(message) {
+      dispatch(setActiveMessage(message));
+    },
+  }),
+)(MessageList);
