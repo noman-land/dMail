e9c5a7d4067524573a58b2226386298300da42e5
Noman <noman@noman.land>
2016-11-22 02:42:39 -0500

    Alphabetize

--- js/actions/messagesActions.js
+++ js/actions/messagesActions.js
@@ -1,29 +1,32 @@
 import { createAction } from 'redux-actions';
 import ActionTypes from './actionTypes/messagesActionTypes';
 
-export const fetchMessagesError = createAction(
-  ActionTypes.MESSAGES_FETCH_ERROR,
-  error => ({error})
-);
-export const fetchMessagesStart = createAction(
-  ActionTypes.MESSAGES_FETCH_START
-);
-export const fetchMessagesSuccess = createAction(
-  ActionTypes.MESSAGES_FETCH_SUCCESS,
-  messages => ({messages})
-);
-
 export const clearActiveMessage = createAction(
   ActionTypes.ACTIVE_MESSAGE_CLEAR
-);
-export const setActiveMessage = createAction(
-  ActionTypes.ACTIVE_MESSAGE_SET,
-  message => ({message})
 );
 
 export const composingMessage = createAction(
   ActionTypes.MESSAGE_COMPOSING,
   isComposing => ({isComposing})
+);
+
+export const fetchMessagesError = createAction(
+  ActionTypes.MESSAGES_FETCH_ERROR,
+  error => ({error})
+);
+
+export const fetchMessagesStart = createAction(
+  ActionTypes.MESSAGES_FETCH_START
+);
+
+export const fetchMessagesSuccess = createAction(
+  ActionTypes.MESSAGES_FETCH_SUCCESS,
+  messages => ({messages})
+);
+
+export const setActiveMessage = createAction(
+  ActionTypes.ACTIVE_MESSAGE_SET,
+  message => ({message})
 );
 
 export const setDraftSubject = createAction(
