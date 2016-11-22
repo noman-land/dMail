094427eb161c2406252d025981871c79ebd7e3a8
Noman <Noman>
2016-11-22 04:41:32 -0500

    Rename action payload from activeMailbox to mailbox

--- js/reducers/activeMailbox.js
+++ js/reducers/activeMailbox.js
@@ -6,7 +6,7 @@ const activeMailbox = (
 ) => {
   switch (action.type) {
     case ActionTypes.MAILBOX_SET:
-      return action.payload.activeMailbox;
+      return action.payload.mailbox;
     default:
       return state;
   }
