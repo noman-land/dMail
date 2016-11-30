be288cd26f3b8617274d31b4809abb0960a587d9
Noman <Noman>
2016-11-30 18:25:42 -0500

    Make clicking on header fetch messages

--- js/components/header/Header.js
+++ js/components/header/Header.js
@@ -2,12 +2,21 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 
+import { getMessages } from '../../actions/asyncActions/messageAsyncActions';
+
 import EthereumOnlineBadge from './EthereumOnlineBadge';
 import IpfsOnlineBadge from './IpfsOnlineBadge';
 
 class Header extends Component {
   constructor(props, context) {
     super(props, context);
+
+    this.handleHeaderClick = this.handleHeaderClick.bind(this);
+  }
+
+  handleHeaderClick() {
+    const { getMessages, primaryAccount } = this.props;
+    getMessages(primaryAccount);
   }
 
   render() {
@@ -15,7 +24,7 @@ class Header extends Component {
     return (
       <div className="header">
         <div className="flex align-items-center">
-          <Link to="/inbox" className="decoration-none">
+          <Link to="/inbox" className="decoration-none" onClick={this.handleHeaderClick}>
             <span className="text-black">
                 dMail
             </span>
@@ -38,11 +47,19 @@ class Header extends Component {
 }
 
 Header.propTypes = {
+  getMessages: PropTypes.func.isRequired,
   messagesLength: PropTypes.number,
+  primaryAccount: PropTypes.string,
 };
 
 export default connect(
   state => ({
-    messagesLength: state.messages.length
-  })
+    messagesLength: state.messages.length,
+    primaryAccount: state.primaryAccount,
+  }),
+  dispatch => ({
+    getMessages(account) {
+      dispatch(getMessages(account));
+    }
+  }),
 )(Header);
