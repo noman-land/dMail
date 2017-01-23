59720f8f0707ede104c0a0f5a5bd1b8804979809
Noman <noman@noman.land>
2017-01-22 22:36:00 -0500

    Centralize app init in main script
    The initialization of the app has been made much cleaner and saner. All
    the important things happen before the initial render and even before
    the setup of the store.
    
    First Ethereum and IPFS go online
    Then the accounts are fetched and a primary account is set
    Then the Redux store is setup with initial state so far
    All the relevant data is fetched like messages of primary account
    And the first render happens.
    
    In the future, all of the failures that could happen above before the
    first render will be handled accordingly.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,26 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Q from 'q';
+
 // Styles
 require("font-awesome-webpack");
 import '../sass/style.sass';
 
-// React/Redux/Router
-import React from 'react';
-import { render } from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+// Redux/Router
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import { Router, Route, Redirect, IndexRedirect, IndexRoute, browserHistory } from 'react-router';
-import rootReducer from './reducers/rootReducer';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { Router, Route, IndexRedirect, IndexRoute, browserHistory } from 'react-router';
+import configureStore from './modules/reduxUtils';
+
+import {
+  init as ethereumInit,
+  getAccounts as ethereumGetAccounts,
+  getCoinbase as ethereumGetCoinbase
+} from './modules/ethereumUtils';
+
+import { init as ipfsInit } from './modules/ipfsUtils';
+
+import { getMessages } from './actions/asyncActions/messageAsyncActions';
 
 // Components
 import App from './components/App';
@@ -19,27 +30,81 @@ import DraftsContainer from './containers/DraftsContainer';
 import InboxContainer from './containers/InboxContainer';
 import SettingsContainer from './containers/SettingsContainer';
 
-const store = createStore(
-  rootReducer,
-  applyMiddleware(thunk)
-);
+class Application {
+  fetchData() {
+    const { dispatch, getState } = this.store;
+    const { primaryAccount } = getState();
+    dispatch(getMessages(primaryAccount));
+    // dispatch();
+    // dispatch();
+  }
 
-window.dMail = {...window.dMail, store};
+  getPrimaryAccount() {
+    const storedPrimaryAccount = localStorage.getItem('primaryAccount');
+    if (!storedPrimaryAccount) {
+      return Q(storedPrimaryAccount);
+    }
 
-render(
-  <Provider store={store}>
-    <Router history={browserHistory}>
-      <Route path="/" component={App}>
-        <IndexRedirect to="/inbox" />
-        <Route path="/inbox" component={MailboxContainer}>
-          <IndexRoute component={InboxContainer} />
-          <Route path="/drafts" component={DraftsContainer} />
-          <Route path="/drafts/:messageId" component={MessageFullContainer} />
-          <Route path="/inbox/:messageId" component={MessageFullContainer} />
-        </Route>
-        <Route path="/settings" component={SettingsContainer} />
-      </Route>
-    </Router>
-  </Provider>,
-  document.getElementById('root')
-);
+    return ethereumGetCoinbase().then((coinbase) => {
+      localStorage.setItem('primaryAccount', coinbase);
+      return coinbase;
+    });
+  }
+
+  goOnline() {
+    return Q.all([
+      ethereumInit(),
+      ipfsInit()
+    ]).spread((networkId, { version: ipfsVersion }) => {
+      return {
+        connectedToEthereum: true,
+        connectedToIpfs: true,
+        ipfsVersion,
+        networkId,
+      };
+    });
+  }
+
+  setupRedux(initialState) {
+    this.store = configureStore(initialState, browserHistory);
+  }
+
+  start() {
+    window.dMail = this;
+
+    this.goOnline().then((initialData) => {
+      ethereumGetAccounts().then((ethereumAccounts) => {
+        this.getPrimaryAccount().then((primaryAccount) => {
+          this.setupRedux({...initialData, ethereumAccounts, primaryAccount});
+          this.fetchData();
+          this.startRouter();
+        }).catch(error => {throw error}).done()
+      }).catch(error => {throw error}).done();
+    }).catch(error => {throw error}).done();
+  }
+
+  startRouter() {
+    const syncedHistory = syncHistoryWithStore(browserHistory, this.store, {
+      selectLocationState: state => state.routing
+    });
+    render(
+      <Provider store={this.store}>
+        <Router history={syncedHistory}>
+          <Route path="/" component={App}>
+            <IndexRedirect to="/inbox" />
+            <Route path="/inbox" component={MailboxContainer}>
+              <IndexRoute component={InboxContainer} />
+              <Route path="/drafts" component={DraftsContainer} />
+              <Route path="/drafts/:messageId" component={MessageFullContainer} />
+              <Route path="/inbox/:messageId" component={MessageFullContainer} />
+            </Route>
+            <Route path="/settings" component={SettingsContainer} />
+          </Route>
+        </Router>
+      </Provider>,
+      document.getElementById('root')
+    );
+  }
+}
+
+new Application().start();
