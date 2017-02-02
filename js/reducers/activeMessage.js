b8b3f65d0e56f6e551005217b2fbdd6bde4e7a2d
Noman <noman@noman.land>
2017-02-02 01:45:20 -0500

    Remove message id

--- js/reducers/activeMessage.js
+++ js/reducers/activeMessage.js
@@ -3,7 +3,6 @@ import ActionTypes from '../actions/actionTypes/messagesActionTypes';
 const activeMessage = (
   state = {
     body: 'It shows up when there isn\'t a real one to show',
-    id: 2,
     sender: '0x0000',
     subject: 'This is a default message',
     sentDate: 0,
