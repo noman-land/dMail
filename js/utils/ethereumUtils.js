6839eca9381e123f51654d8c72ac22ee8575c4fe
Noman <noman@noman.land>
2017-10-25 02:56:36 -0400

    Add DMailInterface and web3 to window object

--- js/utils/ethereumUtils.js
+++ js/utils/ethereumUtils.js
@@ -68,9 +68,9 @@ export const goOnline = () => {
   const deferred = Q.defer();
 
   if (typeof window.web3 !== 'undefined') {
-    web3 = new Web3(window.web3.currentProvider);
+    window.web3 = web3 = new Web3(window.web3.currentProvider);
   } else {
-    web3 = new Web3(new Web3.providers.HttpProvider(DEFAULT_GETH_RPC_PATH));
+    window.web3 = web3 = new Web3(new Web3.providers.HttpProvider(DEFAULT_GETH_RPC_PATH));
   }
 
   // TODO: There must be a better way to check for online status
